73c6e4f274b5eb062d82faaa94e1e518e4752d68
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2017-07-02 15:24:25 +0100

    Add api key param

--- library/todo.py
+++ library/todo.py
@@ -3,10 +3,12 @@
 from ansible.module_utils.basic import *
 
 def main():
-
-    module = AnsibleModule(argument_spec={})
-    response = {"hello": "world"}
-    module.exit_json(changed=False, meta=response)
+	fields = {
+		"api_key": {"required": True, "type": "str"},
+	}
+	module = AnsibleModule(argument_spec=fields)
+	response = {"hello": "world"}
+	module.exit_json(changed=False, meta=module.params)
 
 
 if __name__ == '__main__':
