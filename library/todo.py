028bdc815d5c4aab42d3b0f3e110e8956415b063
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2017-07-02 17:11:32 +0100

    Include todoist api

--- library/todo.py
+++ library/todo.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 
 from ansible.module_utils.basic import *
+from todoist import TodoistAPI
 
 def main():
-	fields = {
-		"api_key": {"required": True, "type": "str"},
-	}
-	module = AnsibleModule(argument_spec=fields)
-	response = {"hello": "world"}
-	module.exit_json(changed=False, meta=module.params)
+    fields = {
+        "api_key": {"required": True, "type": "str"},
+    }
+    module = AnsibleModule(argument_spec=fields)
+
+    doist = TodoistAPI(module.params['api_key'])
+
+    response = {"key": module.params['api_key']}
+    module.exit_json(changed=False, meta=response)
 
 
 if __name__ == '__main__':
