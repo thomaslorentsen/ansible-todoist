06d148470ff1c72379284c3fb48413e8c6dae7d5
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2017-07-18 09:23:11 +0100

    Allow ansible to create a todoist task in a project

--- library/todo.py
+++ library/todo.py
@@ -7,13 +7,26 @@ import todoist
 def main():
     fields = {
         "api_key": {"required": True, "type": "str"},
+        "content": {"required": True, "type": "str"},
+        "project": {"default": "Inbox", "type": "str"},
     }
     module = AnsibleModule(argument_spec=fields)
 
     doist = todoist.TodoistAPI(module.params['api_key'])
+    response = doist.sync(resource_types=['projects'])
+    if 'error' in response:
+        module.fail_json(changed=False, msg=response['error'])
 
-    response = {"key": module.params['api_key']}
-    module.exit_json(changed=False, meta=response)
+    projects = doist.projects.all(lambda x: x['name'] == module.params['project'])
+    project = projects.pop()
+
+    response = doist.items.add(module.params['content'], project['id'])
+
+    response = {"response": response['id']}
+
+    response = doist.commit()
+
+    module.exit_json(changed=True, meta=response)
 
 
 if __name__ == '__main__':
